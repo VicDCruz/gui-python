3aa1bdf2ebbdab0bfde19f63777f88d0cafe7214
Victor Cruz <vicdan215@gmail.com>
2020-06-09 23:30:24 -0500

    Add login window

---
+++ Login.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\Login.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setEnabled(True)
+        MainWindow.resize(413, 418)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setFormAlignment(
+            QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
+        self.formLayout.setContentsMargins(-1, -1, -1, 0)
+        self.formLayout.setVerticalSpacing(7)
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(
+            0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.txtUser = QtWidgets.QLineEdit(self.centralwidget)
+        self.txtUser.setObjectName("txtUser")
+        self.formLayout.setWidget(
+            0, QtWidgets.QFormLayout.FieldRole, self.txtUser)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setTextFormat(QtCore.Qt.AutoText)
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(
+            1, QtWidgets.QFormLayout.LabelRole, self.label_2)
+        self.txtPwd = QtWidgets.QLineEdit(self.centralwidget)
+        self.txtPwd.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.txtPwd.setObjectName("lineEdit")
+        self.formLayout.setWidget(
+            1, QtWidgets.QFormLayout.FieldRole, self.txtPwd)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.checkCredentials)
+        self.formLayout.setWidget(
+            2, QtWidgets.QFormLayout.FieldRole, self.pushButton)
+        self.lblIncorrect = QtWidgets.QLabel(self.centralwidget)
+        self.lblIncorrect.setEnabled(False)
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        self.lblIncorrect.setFont(font)
+        self.lblIncorrect.setObjectName("lblIncorrect")
+        self.formLayout.setWidget(
+            3, QtWidgets.QFormLayout.FieldRole, self.lblIncorrect)
+        self.verticalLayout.addLayout(self.formLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 413, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Usuario"))
+        self.label_2.setText(_translate("MainWindow", "Contraseña"))
+        self.pushButton.setText(_translate("MainWindow", "Ingresar"))
+        # self.lblIncorrect.setText(_translate(
+        #     "MainWindow", "Usuario incorrecto"))
+
+    def checkCredentials(self):
+        if (self.txtUser.text() == 'hola' and self.txtPwd.text() == 'hola'):
+            self.lblIncorrect.setText('Correcto')
+        else:
+            self.lblIncorrect.setText('Datos incorrectos')
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # gui-python
 Explore a new to create a GUI for Windows. Also, building and deploying it.
+
+# Comandos importantes
+- `pyuic5 -x ventana.ui -o ventana_ui.py`
+
+
+# References
+- [Primeros pasos para PyQT5 y Qt Designer](https://medium.com/@hektorprofe/primeros-pasos-en-pyqt-5-y-qt-designer-programas-gr%C3%A1ficos-con-python-6161fba46060)
+- [How to Convert any Python File to .EXE](https://youtu.be/UZX5kH72Yx4)
+- [Install Qt5 Designer](https://youtu.be/FVpho_UiDAY)
+- [Install Python on Windows 10](https://matthewhorne.me/how-to-install-python-and-pip-on-windows-10/#:~:text=Add%20PIP%20to%20the%20Windows%20Environment%20Variables&text=Next%20select%20Environmental%20Variables%20and,New%20and%20type%20the%20following.&text=It%20should%20look%20something%20like,on%20all%20the%20opened%20windows.)
+- [Install Anaconda](https://medium.com/@hektorprofe/tutorial-windows-10-agregar-el-python-de-anaconda-al-path-para-utilizarlo-en-la-cmd-y-powershell-72acf22901a)
