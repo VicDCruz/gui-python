f2081a8b90e558aab1b41b2a135090b0bccc2c38
Victor Cruz <vicdan215@gmail.com>
2020-06-29 09:56:31 -0500

    Add labels & finish last table

--- Dashboard.py
+++ Dashboard.py
@@ -10,18 +10,26 @@ from PyQt5 import QtWebEngineWidgets
 from PyQt5 import QtCore, QtGui, QtWidgets
 import pyodbc
 import logging
+import plotly
+import plotly.graph_objs as go
+import sys
 
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+sys.argv.append("--disable-web-security")
+path = QtCore.QDir.current().filePath('./plotly-latest.min.js')
+local = QtCore.QUrl.fromLocalFile(path).toString()
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(841, 840)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
         MainWindow.setMinimumSize(QtCore.QSize(0, 100))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -29,10 +37,12 @@ class Ui_MainWindow(object):
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.label.sizePolicy().hasHeightForWidth())
         self.label.setSizePolicy(sizePolicy)
         self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
@@ -40,32 +50,48 @@ class Ui_MainWindow(object):
         self.ddlNameProcessBook.setObjectName("ddlNameProcessBook")
         self.gridLayout.addWidget(self.ddlNameProcessBook, 0, 1, 1, 1)
         self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.scrollArea.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(
+            self.scrollArea.sizePolicy().hasHeightForWidth())
         self.scrollArea.setSizePolicy(sizePolicy)
         self.scrollArea.setMinimumSize(QtCore.QSize(0, 350))
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 796, 936))
+        self.scrollAreaWidgetContents.setGeometry(
+            QtCore.QRect(0, -657, 796, 1005))
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout = QtWidgets.QVBoxLayout(
+            self.scrollAreaWidgetContents)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.tableTotalCount = QtWidgets.QTableWidget(self.scrollAreaWidgetContents)
+        self.label_2 = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2)
+        self.tableTotalCount = QtWidgets.QTableWidget(
+            self.scrollAreaWidgetContents)
         self.tableTotalCount.setMinimumSize(QtCore.QSize(0, 300))
         self.tableTotalCount.setObjectName("tableTotalCount")
         self.tableTotalCount.setColumnCount(0)
         self.tableTotalCount.setRowCount(0)
         self.verticalLayout.addWidget(self.tableTotalCount)
-        self.tableBigData = QtWidgets.QTableWidget(self.scrollAreaWidgetContents)
+        self.label_3 = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout.addWidget(self.label_3)
+        self.tableBigData = QtWidgets.QTableWidget(
+            self.scrollAreaWidgetContents)
         self.tableBigData.setMinimumSize(QtCore.QSize(0, 300))
         self.tableBigData.setObjectName("tableBigData")
         self.tableBigData.setColumnCount(0)
         self.tableBigData.setRowCount(0)
         self.verticalLayout.addWidget(self.tableBigData)
-        self.tableFullTotalCount = QtWidgets.QTableWidget(self.scrollAreaWidgetContents)
+        self.label_4 = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.label_4.setObjectName("label_4")
+        self.verticalLayout.addWidget(self.label_4)
+        self.tableFullTotalCount = QtWidgets.QTableWidget(
+            self.scrollAreaWidgetContents)
         self.tableFullTotalCount.setMinimumSize(QtCore.QSize(0, 300))
         self.tableFullTotalCount.setObjectName("tableFullTotalCount")
         self.tableFullTotalCount.setColumnCount(0)
@@ -98,6 +124,9 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "Nombre de ProcesoLibro"))
+        self.label_2.setText(_translate("MainWindow", "Cuenta total"))
+        self.label_3.setText(_translate("MainWindow", "Gran dato"))
+        self.label_4.setText(_translate("MainWindow", "Cuenta total desglosada"))
 
     def connectToDb(self):
         self.logger.info("Connection to DB")
@@ -142,34 +171,54 @@ class Ui_MainWindow(object):
         table.setHorizontalHeaderLabels(headers)
         for i in range(len(data)):
             for j in range(len(headers)):
-                table.setItem(i, j, QtWidgets.QTableWidgetItem(str(data[i][j])))
+                table.setItem(
+                    i, j, QtWidgets.QTableWidgetItem(str(data[i][j])))
 
     def populateDashboard(self):
-        headerTotalCount = self.getColumn(self.exec(r"""select distinct DoctoFront
-            from SuperConsultaUsos
-            where NombreProcesoLibro = '{0}'
-            order by DoctoFront""".format(self.ddlNameProcessBook.currentText())), 0)
-        dataTotalCount = self.exec(r"""
-            select ClasificaciondeGranDato, DoctoFront, count(*) 
-            from SuperConsultaUsos 
-            where NombreProcesoLibro = '{0}' 
-            group by ClasificaciondeGranDato, DoctoFront
-            order by ClasificaciondeGranDato;
-        """.format(self.ddlNameProcessBook.currentText()))
-        transformedData = self.transform(dataTotalCount, headerTotalCount)
-        headerTotalCount.insert(0, "Clasificación de Gran Dato")
-        headerTotalCount.append("Total general")
-        if (headerTotalCount[1] is None):
-            headerTotalCount[1] = '(En blanco)'
-        self.populateTable(self.tableTotalCount, headerTotalCount, transformedData)
-        headerBigData = ('Gran dato', 'Cuenta total')
-        dataBigData = self.exec(r"""
-            select GranDato, count(*)
-            from SuperConsultaUsos
-            where NombreProcesoLibro = '{0}'
-            group by GranDato
-        """.format(self.ddlNameProcessBook.currentText()))
-        self.populateTable(self.tableBigData, headerBigData, dataBigData)
+        if (self.ddlNameProcessBook.currentText() != "Selecciona un dato"):
+            headerTotalCount = self.getColumn(self.exec(r"""select distinct DoctoFront
+                from SuperConsultaUsos
+                where NombreProcesoLibro = '{0}'
+                order by DoctoFront""".format(self.ddlNameProcessBook.currentText())), 0)
+            dataTotalCount = self.exec(r"""
+                select ClasificaciondeGranDato, DoctoFront, count(*) 
+                from SuperConsultaUsos 
+                where NombreProcesoLibro = '{0}' 
+                group by ClasificaciondeGranDato, DoctoFront
+                order by ClasificaciondeGranDato;
+            """.format(self.ddlNameProcessBook.currentText()))
+            transformedData = self.transform(dataTotalCount, headerTotalCount)
+            headerTotalCount.insert(0, "Clasificación de Gran Dato")
+            headerTotalCount.append("Total general")
+            if (headerTotalCount[1] is None):
+                headerTotalCount[1] = '(En blanco)'
+            self.populateTable(self.tableTotalCount,
+                               headerTotalCount, transformedData)
+
+            headerBigData = ('Gran dato', 'Cuenta total')
+            dataBigData = self.exec(r"""
+                select GranDato, count(*)
+                from SuperConsultaUsos
+                where NombreProcesoLibro = '{0}'
+                group by GranDato
+            """.format(self.ddlNameProcessBook.currentText()))
+            self.populateTable(self.tableBigData, headerBigData, dataBigData)
+
+            headerFullTotal = ['Destino', 'Origen', 'Residencia']
+            dataFullTotal = self.exec(r"""
+                SELECT ListaTotal, Uso, count(*)
+                from SuperConsultaUsos
+                where NombreProcesoLibro = 'Libro 360' AND 
+                (Uso = 'Destino' OR Uso = 'Origen' OR Uso = 'Residencia')
+                group by ListaTotal, Uso;
+            """)
+            transformedData = self.transform(dataFullTotal, headerFullTotal)
+            headerFullTotal.insert(1, 'Dato')
+            headerFullTotal.append('Total general')
+            self.populateTable(self.tableFullTotalCount,
+                               headerFullTotal, transformedData)
+
+            self.displayPie()
 
     def transform(self, data, headers):
         output = []
@@ -200,6 +249,35 @@ class Ui_MainWindow(object):
             tmp.append(total)
             output.append(tmp)
         return output
+
+    def displayPie(self):
+        data = self.exec(r"""
+            select ClasificaciondeGranDato, count(*)
+            from SuperConsultaUsos 
+            where NombreProcesoLibro = '{0}'
+            group by ClasificaciondeGranDato;
+        """.format(self.ddlNameProcessBook.currentText()))
+        fig = go.Figure(go.Pie(
+            labels=self.getColumn(data, 0),
+            values=self.getColumn(data, 1),
+        ))
+        self.show(fig, "Súper consulta")
+
+    def show(self, fig=None, title=""):
+        raw_html = '<html><head><meta charset="utf-8" />'
+        raw_html += '<script src="{}"></script></head>'.format(local)
+        raw_html += '<body>'
+        if (fig is not None):
+            margin = dict(l=0, r=0, t=25)
+            if (title != ""):
+                fig.update_layout(margin=margin, title_text=title)
+            else:
+                fig.update_layout(margin=margin)
+            raw_html += plotly.offline.plot(fig,
+                                            include_plotlyjs=False, output_type='div')
+        raw_html += '</body></html>'
+        self.graph.setHtml(raw_html)
+        self.graph.show()
 
 
 if __name__ == "__main__":
