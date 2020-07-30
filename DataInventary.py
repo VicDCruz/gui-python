f4d52bcd1dce842146fed01d47d3d20e7f3fc468
Victor Cruz <vicdan215@gmail.com>
2020-07-29 22:51:02 -0500

    Changes to Inventary

--- DataInventary.py
+++ DataInventary.py
@@ -19,15 +19,34 @@ sys.argv.append("--disable-web-security")
 path = QtCore.QDir.current().filePath('./plotly-latest.min.js')
 local = QtCore.QUrl.fromLocalFile(path).toString()
 
+QUERYBIGDATA = r"""
+    SELECT NombreProcesoLibro, GranDato, COUNT(*)
+    FROM qryInventarioGeneral_P02
+    WHERE GranDato <> ''
+        AND NombreProcesoLibro = '{0}'
+    GROUP BY NombreProcesoLibro, GranDato
+"""
+
+QUERYTOTALLIST = r"""
+    SELECT GranDato, ListaTotal, COUNT(*)
+    FROM qryInventarioGeneral_P02
+    WHERE ListaTotal <> ''
+        AND NombreProcesoLibro = '{0}'
+    GROUP BY GranDato, ListaTotal
+"""
+
 
 class DataInventary(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 800)
+        MainWindow.resize(978, 791)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
+        self.btnClean = QtWidgets.QPushButton(self.centralwidget)
+        self.btnClean.setObjectName("btnClean")
+        self.gridLayout.addWidget(self.btnClean, 3, 0, 1, 1)
         self.label = QtWidgets.QLabel(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(
             QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Preferred)
@@ -38,14 +57,6 @@ class DataInventary(object):
         self.label.setSizePolicy(sizePolicy)
         self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-
-        self.btnClean = QtWidgets.QPushButton(self.centralwidget)
-        self.btnClean.setObjectName("btnClean")
-        self.gridLayout.addWidget(self.btnClean, 2, 0, 1, 1)
-
-        self.ddlValues = QtWidgets.QComboBox(self.centralwidget)
-        self.ddlValues.setObjectName("ddlValues")
-        self.gridLayout.addWidget(self.ddlValues, 0, 1, 1, 1)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.txtUse = QtWidgets.QTableWidget(self.centralwidget)
@@ -71,10 +82,19 @@ class DataInventary(object):
         self.graph.setUrl(QtCore.QUrl("about:blank"))
         self.graph.setObjectName("graph")
         self.horizontalLayout.addWidget(self.graph)
-        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 2)
+        self.gridLayout.addLayout(self.horizontalLayout, 2, 0, 1, 3)
+        self.ddlValues = QtWidgets.QComboBox(self.centralwidget)
+        self.ddlValues.setObjectName("ddlValues")
+        self.gridLayout.addWidget(self.ddlValues, 0, 1, 1, 2)
+        self.btnContract = QtWidgets.QPushButton(self.centralwidget)
+        self.btnContract.setObjectName("btnContract")
+        self.gridLayout.addWidget(self.btnContract, 4, 2, 1, 1)
+        self.btnExpand = QtWidgets.QPushButton(self.centralwidget)
+        self.btnExpand.setObjectName("btnExpand")
+        self.gridLayout.addWidget(self.btnExpand, 4, 1, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 100, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 978, 26))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -129,26 +149,34 @@ class DataInventary(object):
         self.menuMenu.addAction(self.actionDataInventary)
         self.menubar.addAction(self.menuMenu.menuAction())
 
+        self.circle = 0
         self.startLog()
         self.connectToDb()
         self.populateAll()
+        self.btnContract.clicked.connect(self.contract)
+        self.btnContract.setEnabled(False)
+        self.btnExpand.clicked.connect(self.expand)
+        self.btnExpand.setEnabled(False)
         self.btnClean.clicked.connect(self.cleanAtributes)
-        self.ddlValues.currentTextChanged.connect(self.getUse)
+        self.ddlValues.currentTextChanged.connect(self.display)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Inventario de datos"))
+        MainWindow.setWindowTitle(_translate(
+            "MainWindow", "Inventario de datos"))
         MainWindow.setWindowIcon(QtGui.QIcon('favicon.ico'))
 
         self.btnClean.setText(_translate(
             "MainWindow", "Limpiar búsqueda"))
-
         self.label.setText(_translate("MainWindow", "Procesos o Libros"))
+        self.btnContract.setText(_translate("MainWindow", "Contraer"))
+        self.btnExpand.setText(_translate("MainWindow", "Expandir"))
         self.menuMenu.setTitle(_translate("MainWindow", "Menu"))
         self.actionInicio.setText(_translate("MainWindow", "Inicio"))
-        self.actionConsulta.setText(_translate("MainWindow", "Gráfica de grandes datos en procesos y libros"))
+        self.actionConsulta.setText(_translate(
+            "MainWindow", "Gráfica de grandes datos en procesos y libros"))
         self.actionBusqueda.setText(_translate("MainWindow", "Búsqueda"))
         self.actionDatoRegulado.setText(
             _translate("MainWindow", "Usos por dato regulado"))
@@ -213,16 +241,32 @@ class DataInventary(object):
         self.txtUse.setRowCount(0)
         self.populateAll()
 
+    def display(self):
+        self.circle = 0
+        self.getUse()
+
     def getUse(self):
-        headers = ('Libro o Procesos', 'Nombre del proceos o libro',
-                   'Gran dato', 'Total')
-        query = r"""
-                SELECT SuperConsultaUsos.LibroProceso, SuperConsultaUsos.NombreProcesoLibro, 
-                        SuperConsultaUsos.GranDato, Count(SuperConsultaUsos.ListaTotal)
-                FROM SuperConsultaUsos
-                where NombreProcesoLibro = '{0}'
-                GROUP BY SuperConsultaUsos.LibroProceso, SuperConsultaUsos.NombreProcesoLibro, SuperConsultaUsos.GranDato
-            """.format(self.ddlValues.currentText())
+        # headers = ('Libro o Procesos', 'Nombre del proceos o libro',
+        #            'Gran dato', 'Total')
+        # SELECT SuperConsultaUsos.LibroProceso, SuperConsultaUsos.NombreProcesoLibro, 
+        #         SuperConsultaUsos.GranDato, Count(SuperConsultaUsos.ListaTotal)
+        # FROM SuperConsultaUsos
+        # where NombreProcesoLibro = '{0}'
+        # GROUP BY SuperConsultaUsos.LibroProceso, SuperConsultaUsos.NombreProcesoLibro, SuperConsultaUsos.GranDato
+        if (self.circle == 0):
+            headers = ('Nombre del proceos o libro', 'Gran dato', 'Total')
+            query = r"""
+                SELECT *
+                FROM qryInventarioGeneral_ProcesoL
+                WHERE NombreProcesoLibro='{0}'
+                """.format(self.ddlValues.currentText())
+        elif (self.circle == 1):
+            headers = ('Nombre del proceos o libro', 'Gran dato', 'Lista Total')
+            query = r"""
+                SELECT *
+                FROM qryInventarioGeneral_P02
+                WHERE NombreProcesoLibro='{0}'
+                """.format(self.ddlValues.currentText())
         if (self.ddlValues.currentText() == 'Todos'):
             headers = ('Gran dato', 'Total')
             query = r"""
@@ -239,7 +283,14 @@ class DataInventary(object):
             for j in range(len(data)):
                 self.txtUse.setItem(
                     j, i, QtWidgets.QTableWidgetItem(str(data[j][i])))
-        self.displayPie()
+        if (self.ddlValues.currentText() == 'Todos'):
+            self.btnExpand.setEnabled(False)
+            self.btnContract.setEnabled(False)
+            self.displayPie()
+        else:
+            self.btnExpand.setEnabled(True)
+            self.btnContract.setEnabled(True)
+            self.modifyCircles()
 
     def summary(self, headers, row):
         output = ""
@@ -252,29 +303,53 @@ class DataInventary(object):
         query = r"""
             SELECT SuperConsultaUsos.GranDato, Count(SuperConsultaUsos.ListaTotal)
             FROM SuperConsultaUsos
-            where NombreProcesoLibro = '{0}'
-            GROUP BY SuperConsultaUsos.LibroProceso, SuperConsultaUsos.NombreProcesoLibro, SuperConsultaUsos.GranDato
+            WHERE GranDato <> ''
+            GROUP BY SuperConsultaUsos.GranDato
         """.format(self.ddlValues.currentText())
-        if (self.ddlValues.currentText() == 'Todos'):
-            query = r"""
-                SELECT SuperConsultaUsos.GranDato, Count(SuperConsultaUsos.ListaTotal)
-                FROM SuperConsultaUsos
-                WHERE GranDato <> ''
-                GROUP BY SuperConsultaUsos.GranDato
-            """.format(self.ddlValues.currentText())
         data = self.exec(query)
         fig = go.Figure(go.Pie(
             labels=self.getColumn(data, 0),
             values=self.getColumn(data, 1),
         ))
-        self.show(fig, "Consulta en pie")
+        self.show(fig, "Consulta en pie de todos los libros y procesos")
+
+    def modifyCircles(self):
+        processbook = self.ddlValues.currentText()
+        if (processbook != 'Selecciona un dato'):
+            if (self.circle >= 0):
+                processes = self.exec(QUERYBIGDATA.format(processbook))
+                parents = [""] + self.getColumn(processes, 0)
+                names = [processbook] + self.getColumn(processes, 1)
+                counts = self.getColumn(processes, 2)
+                total = self.getTotal(counts)
+                values = [total] + counts
+                title = 'Grandes datos de {0}'.format(processbook)
+                if (self.circle >= 1):
+                    tables = self.exec(QUERYTOTALLIST.format(processbook))
+                    parents = parents + self.getColumn(tables, 0)
+                    newValues = self.getColumn(tables, 2)
+                    if (self.circle == 1):
+                        names = names + \
+                            self.getPercentages(self.getColumn(
+                                tables, 1), newValues, total)
+                    else:
+                        names = names + self.getColumn(tables, 1)
+                    values = values + newValues
+                    title = 'Lista total de {0}'.format(processbook)
+                fig = go.Figure(go.Sunburst(
+                    labels=names,
+                    parents=parents,
+                    values=values,
+                    branchvalues="total",
+                ))
+                self.show(fig, title)
 
     def show(self, fig=None, title=""):
         raw_html = '<html><head><meta charset="utf-8" />'
         raw_html += '<script src="{}"></script></head>'.format(local)
         raw_html += '<body>'
         if (fig is not None):
-            margin = dict(l=0, r=0, t=25)
+            margin = dict(t=25, l=0, r=0, b=0)
             if (title != ""):
                 fig.update_layout(margin=margin, title_text=title)
             else:
@@ -284,6 +359,29 @@ class DataInventary(object):
         raw_html += '</body></html>'
         self.graph.setHtml(raw_html)
         self.graph.show()
+
+    def getTotal(self, a):
+        output = 0
+        for element in a:
+            output += element
+        return output
+
+    def getPercentages(self, names, values, total):
+        count = 0
+        for value in values:
+            names[count] += "<br>{:.1f} %".format(value / total * 100)
+            count += 1
+        return names
+
+    def expand(self):
+        if (self.circle < 1):
+            self.circle += 1
+            self.getUse()
+
+    def contract(self):
+        if (self.circle > 0):
+            self.circle -= 1
+            self.getUse()
 
     def setOptionsMenu(self, toMenu, toRegulatedData, toGraph, toSearchByWord,
                        toDashboard, toAllData, toResidenceBigData,
